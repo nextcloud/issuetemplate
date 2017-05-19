7e94404e98071ee42b64a49244cbb5bb2cb2d8fb
Philip Lundqvist <philiplundqvist@gmail.com>
2017-05-19 23:16:33 +0200

    Improved markdown styling and readability
    - Better listing of loaded PHP-modules.
    - Divided Enabled and Disabled apps into different `<details>` blocks.
    - Easier to read rendered code due to better usage of whitespace.
    - Fixed various broken styling.

--- templates/issuetemplate.php
+++ templates/issuetemplate.php
@@ -1,56 +1,77 @@
 ### Server configuration
-**Operating system**: <?php p($_['os']); ?>
+#### Operating system:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['os']); ?>
 
+#### Web server:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['webserver']); ?>
 
-**Web server:** <?php p($_['webserver']); ?>
+#### Database:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['dbserver']); ?>
 
+#### PHP version:
+<?php
+        $parts = explode ('Modules loaded: ', $_['php']);
+        $phpVersion = $parts[0];
+        $modules = explode (', ', $parts[1]); ?>
 
-**Database:** <?php p($_['dbserver']); ?>
-
-
-**PHP version:** <?php p($_['php']); ?>
-
-
-**Nextcloud version:** <?php p($_['version']); ?>
-
-
-**Updated from an older Nextcloud/ownCloud or fresh install:**
-
-**Where did you install Nextcloud from:** <?php p($_['installMethod']); ?>
-
-
-**Signing status:**
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($phpVersion); ?>
 <details>
-	<summary>Signing status</summary>
+        <summary>Modules loaded:</summary>
+
+```
+<?php
+        foreach ($modules as $module) {
+                p(" - " . $module . "\n");
+        } ?>
+```
+</details>
+
+#### Nextcloud version:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['version']); ?>
+
+#### Updated from an older Nextcloud/ownCloud or fresh install:
+&nbsp;&nbsp;&nbsp;&nbsp;YOUR ANSWER HERE
+
+#### Where did you install Nextcloud from:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['installMethod']); ?> YOUR ANSWER HERE
+
+#### Signing status:
+<details>
+        <summary>Signing status</summary>
 
 ```
 <?php print_unescaped(print_r(json_encode($_['integrity'], JSON_PRETTY_PRINT), true)); ?>
 
 ```
-
 </details>
 
-**List of activated apps:**
-<details>
-	<summary>App list</summary>
-```
-Enabled:
-<?php
-	foreach ($_['apps']['enabled'] as $name => $version) {
-		p(" - " . $name . ": " . $version . "\n");
-	} ?>
+#### List of installed apps:
 
-Disabled:
+<details>
+        <summary>Enabled:</summary>
+
+```
 <?php
-	foreach ($_['apps']['disabled'] as $name => $version) {
-		p(" - " . $name . "\n");
-	} ?>
+        foreach ($_['apps']['enabled'] as $name => $version) {
+                p(" - " . $name . ": " . $version . "\n");
+        } ?>
 ```
 </details>
 
-**The content of config/config.php:**
 <details>
-	<summary>Config report</summary>
+        <summary>Disabled:</summary>
+
+```
+<?php
+        foreach ($_['apps']['disabled'] as $name => $version) {
+                p(" - " . $name . "\n");
+        } ?>
+```
+</details>
+
+#### The content of config/config.php:
+<details>
+        <summary>Config report</summary>
 
 ```
 <?php print_unescaped(print_r(json_encode($_['config'], JSON_PRETTY_PRINT), true)); ?>
@@ -58,64 +79,69 @@ Disabled:
 ```
 </details>
 
-**Are you using external storage, if yes which one:** <?php print_unescaped(print_r($_['external'], true)); ?>
+#### Are you using external storage, if yes which one:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php print_unescaped(print_r($_['external'], true)); ?>
 
+#### Are you using encryption:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['encryption']); ?>
 
-**Are you using encryption:** <?php p($_['encryption']); ?>
-
-
-**Are you using an external user-backend, if yes which one:** LDAP/ActiveDirectory/Webdav/...
+#### Are you using an external user-backend, if yes which one:
+&nbsp;&nbsp;&nbsp;&nbsp; YOUR ANSWER HERE (LDAP/ActiveDirectory/Webdav/...)
 
 <?php if(array_key_exists('user_ldap', $_['apps']['enabled'])) { ?>
 #### LDAP configuration (delete this part if not used)
 <details>
-	<summary>LDAP config</summary>
+        <summary>LDAP config</summary>
 
-	```
-	With access to your command line run e.g.:
-	sudo -u www-data php occ ldap:show-config
-	from within your Nextcloud installation folder
+```
+With access to your command line run e.g.:
+sudo -u www-data php occ ldap:show-config
+from within your Nextcloud installation folder
 
-	Without access to your command line download the data/owncloud.db to your local
-	computer or access your SQL server remotely and run the select query:
-	SELECT * FROM `oc_appconfig` WHERE `appid` = 'user_ldap';
+Without access to your command line download the data/owncloud.db to your local
+computer or access your SQL server remotely and run the select query:
+SELECT * FROM `oc_appconfig` WHERE `appid` = 'user_ldap';
 
-
-	Eventually replace sensitive data as the name/IP-address of your LDAP server or groups.
-	```
+Eventually replace sensitive data as the name/IP-address of your LDAP server or groups.
+```
 </details>
 <?php } ?>
+
 ### Client configuration
-**Browser:** <?php p($_['browser']); ?>
+#### Browser:
+&nbsp;&nbsp;&nbsp;&nbsp;<?php p($_['browser']); ?>
 
-
-**Operating system:**
+#### Operating system:
+&nbsp;&nbsp;&nbsp;&nbsp;YOUR ANSWER HERE
 
 ### Logs
 #### Web server error log
 <details>
-	<summary>Web server error log</summary>
-	```
-	Insert your webserver log here
-	```
+        <summary>Web server error log</summary>
+
+```
+Insert your webserver log here
+```
 </details>
 
 #### Nextcloud log (data/nextcloud.log)
 <details>
-	<summary>Nextcloud log</summary>
-	```
-	Insert your Nextcloud log here
-	```
+        <summary>Nextcloud log</summary>
+
+```
+Insert your Nextcloud log here
+```
 </details>
 
 #### Browser log
 <details>
-	<summary>Browser log</summary>
-	```
-	Insert your browser log here, this could for example include:
+        <summary>Browser log</summary>
 
-	a) The javascript console log
-	b) The network log
-	c) ...
-	```
+```
+Insert your browser log here, this could for example include:
+
+a) The javascript console log
+b) The network log
+c) ...
+```
 </details>
